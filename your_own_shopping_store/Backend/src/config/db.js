2e36a02112f97b20582d3304ebf2f59c57d98cbf
mohitkumarsuthar <mohitkumarstudent1234@gmail.com>
2026-04-29 02:01:26 +0530

    Update db.js

--- your_own_shopping_store/Backend/src/config/db.js
+++ your_own_shopping_store/Backend/src/config/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 import pg from "pg";
 
-dotenv.config({ override: false })
+dotenv.config()
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
